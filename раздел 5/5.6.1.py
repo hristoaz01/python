004e7cdbfac7ae2e665f5fd2b81bed8581f18dfe
hristoaz01 <40625226+hristoaz01@users.noreply.github.com>
2022-11-30 21:28:08 +0700

    Update 5.6.1.py

--- раздел 5/5.6.1.py
+++ раздел 5/5.6.1.py
@@ -1,10 +1,9 @@
-num = inpuwhile 1:
-    value = abs(int(input("Введите ваше число: ")))
-    if (value % 3 == 0) and (value % 5 == 0):
-        print("Fizz Buzz")
-    elif value % 3 == 0:
-        print("Fizz")
-    elif value % 5 == 0:
-        print("Buzz")
-    else:
-        print(value)
+value = int(input("Введите ваше число: "))
+if (value % 3 == 0) and (value % 5 == 0):
+    print("Fizz Buzz")
+elif value % 3 == 0:
+    print("Fizz")
+elif value % 5 == 0:
+    print("Buzz")
+else:
+    print(value)
