3b23e1a306fba684a9e5a86143ae65f1fd1d6aae
hristoaz01 <40625226+hristoaz01@users.noreply.github.com>
2022-11-30 21:29:32 +0700

    Update ma5.6.4in.py

--- раздел 5/ma5.6.4in.py
+++ раздел 5/ma5.6.4in.py
@@ -1,4 +1,4 @@
 text = "How are you? Eh, ok. Low or Lower? Ohhh."
 for i in range(len(text)):
-    if (ord(text[i]) >=65) & (ord(text[i])<=90):
-        print(text[i], end="")
+    if text[i].isupper():
+        print(text[i], end='')
