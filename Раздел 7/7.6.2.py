1dc1c63db082a640a529932d04395714b1e2e9e4
hristoaz01 <40625226+hristoaz01@users.noreply.github.com>
2022-12-08 18:13:04 +0700

    Update 7.6.2.py

--- Раздел 7/7.6.2.py
+++ Раздел 7/7.6.2.py
@@ -1,4 +1,13 @@
-import operator
-rates = {'Otkritie': 61.85, 'Sberbank': 59.39, 'VTB': 60.1, 'Gazprombank': 60.2, 'Alpha-Bank': 60}
-x = len(rates)
-print(max(rates.items(), key=operator.itemgetter(1)))
+x = 2199
+
+roman_numeral = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500,
+                 'CM': 900, 'M': 1000}
+keys = list(roman_numeral.keys())
+i = 12
+while x:
+    div = x // roman_numeral[keys[i]]
+    x %= roman_numeral[keys[i]]
+    while div:
+        print(keys[i], end="")
+        div -= 1
+    i -= 1
