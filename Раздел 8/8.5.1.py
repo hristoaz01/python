f3533bdc457f2fe51a7cabea0dfed1aef8ac211f
hristoaz01 <40625226+hristoaz01@users.noreply.github.com>
2022-12-08 18:22:30 +0700

    Update 8.5.1.py

--- Раздел 8/8.5.1.py
+++ Раздел 8/8.5.1.py
@@ -1,19 +1,12 @@
-import re
-text = "hello, word of word"
-chars_popularity = {}
-words_popularity = {}
-
-for i in range(int(len(text))):
-    if text[i].isalpha():
-        chars_popularity[text[i]] = 0
-for i in range(int(len(text))):
-    if text[i].isalpha():
-        chars_popularity[text[i]] += 1
-print(chars_popularity)
-words = re.findall(r'\w+', text)
-
-for i in range (int(len(words))):
-    words_popularity[words[i]] = 0
-for i in range(int(len(words))):
-    words_popularity[words[i]] += 1
-print (words_popularity)
+list = [0, 9, 4, 6, 3, 9, 4, 0, 2, 3, 9]
+print(list, end=' -> ')
+dict = {}
+for x in list:
+    if x in dict.keys():
+        dict[x] = 0
+    else:
+        dict[x] = 1
+for x in list:
+    if dict[x] == 1:
+        list.remove(x)
+print(list)
